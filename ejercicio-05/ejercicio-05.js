eeb5bdb3b93a60b2dfa0eef8d8840c244c77c8b1
Martina Garcia <mgarcia.06@outlook.es>
2025-05-21 13:22:37 -0300

    add ejercicio 5

---
+++ ejercicio-05/ejercicio-05.js
@@ -0,0 +1,18 @@
+const botonArea = document.querySelector("button")
+
+function calcularArea() {
+    const ladoA = document.getElementById("ladoa").value
+    const ladoB = document.getElementById("ladob").value
+    const ladoC = document.getElementById("ladoc").value
+
+    const area = ((ladoA - ladoC) * ladoB) / 2 + ladoB * ladoC
+
+    if (!ladoA || !ladoB || !ladoC) {
+        alert("Los valores ingresados no son válidos")
+        return
+    } else {
+        alert(`El área del terreno es de ${area.toFixed(2)} metros`)
+    }
+}
+
+botonArea.addEventListener("click", calcularArea)
